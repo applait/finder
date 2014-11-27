056f1dbf7fe86518e9318b8673f814b58ce81224
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-27 19:51:46 +0530

    Pass home as the default path if no location.hash is present
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", function () {
     // Initiate the app with a config. This will instantiate the api and invoke all
     // module registrations
     app({
-        path: location.hash.slice(2),
+        path: location.hash ? location.hash.slice(2) : "home",
         root: document.querySelector("body"),
         searchinput: document.querySelector("#searchinput"),
         debug: true
