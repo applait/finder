b81b1e8771d1ac5229bb95b639a964ed909e3a36
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-28 16:01:57 +0530

    Pass all init elems as string of id
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/init.js
+++ js/init.js
@@ -1,17 +1,22 @@
 window.addEventListener("DOMContentLoaded", function () {
 
-    var searchinput = document.querySelector("#topnav-input");
+    var searchinput = "#topnav-input";
 
     // Initiate the app with a config. This will instantiate the api and invoke all
     // module registrations
     app({
         path: location.hash ? location.hash.slice(2) : "home",
         root: document.querySelector("body"),
+        topnav: "#topnav",
+        bottomnav: "#bottomnav",
         searchinput: searchinput,
+        searchbtn: "#searchbtn",
+        cancelbtn: "#cancelbtn",
+        resetbtn: "#resetbtn",
         debug: true
     });
 
     // Set focus on the searchinput
-    searchinput.focus();
+    document.querySelector(searchinput).focus();
 
 });
