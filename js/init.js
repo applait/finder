763e343f92048eeeba0851a1b403289d131d1acb
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-28 12:52:07 +0530

    Put focus in searchinput on page load
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/init.js
+++ js/init.js
@@ -1,12 +1,17 @@
 window.addEventListener("DOMContentLoaded", function () {
 
+    var searchinput = document.querySelector("#topnav-input");
+
     // Initiate the app with a config. This will instantiate the api and invoke all
     // module registrations
     app({
         path: location.hash ? location.hash.slice(2) : "home",
         root: document.querySelector("body"),
-        searchinput: document.querySelector("#topnav-input"),
+        searchinput: searchinput,
         debug: true
     });
+
+    // Set focus on the searchinput
+    searchinput.focus();
 
 });
