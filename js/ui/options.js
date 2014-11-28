a5b3d4add01c98b89bc8ce054642da69c00e5ffd
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-28 16:02:27 +0530

    Add delegated listeners to topnav and bottomnav for actions
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/ui/options.js
+++ js/ui/options.js
@@ -3,12 +3,60 @@
     // Add behaviour for option buttons
     app(function (api) {
 
-        var resetbtn = document.querySelector("#resetbtn");
+        var topnav = document.querySelector(api.args.topnav),
+            bottomnav = document.querySelector(api.args.bottomnav);
 
-        resetbtn.addEventListener("click", function () {
+        /**
+         * Actions to perform when reset button is clicked
+         */
+        var resetbtn_action = function () {
+            var searchinput = document.querySelector(api.args.searchinput);
             api.reset();
-            api.args.searchinput.value = "";
-            api.args.searchinput.focus();
+            searchinput.value = "";
+            searchinput.focus();
+        };
+
+        /**
+         * Actions to perform when search button is clicked
+         */
+        var searchbtn_action = function () {
+            api.search(document.querySelector(api.args.searchinput).value.trim());
+        };
+
+        /**
+         * Bind delegated listeners for elements in topnav
+         */
+        topnav.addEventListener("click", function (e) {
+
+            if (e.target && e.target.id) {
+
+                switch ("#" + e.target.id) {
+
+                case api.args.searchbtn:
+                    searchbtn_action();
+                    break;
+
+                };
+            }
+
+        }, false);
+
+        /**
+         * Bind delegated listeners for elements in bottomnav
+         */
+        bottomnav.addEventListener("click", function (e) {
+
+            if (e.target && e.target.id) {
+
+                switch ("#" + e.target.id) {
+
+                case api.args.resetbtn:
+                    resetbtn_action();
+                    break;
+
+                };
+            }
+
         }, false);
 
     });
