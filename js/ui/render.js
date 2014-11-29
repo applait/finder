e91fa8cc05ee0e49fb04eb8b745fca2561d07275
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-29 14:03:16 +0530

    Add support for default topnav loading
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/ui/render.js
+++ js/ui/render.js
@@ -5,7 +5,7 @@
         // Perform renders required before:load
         api.on("before:load", function (path) {
 
-            var topnavtmpl = $("#tmpl-topnav-" + path);
+            var topnavtmpl = $("#tmpl-topnav-" + path.split("/")[0]) || $("#tmpl-topnav-default");
 
             if (topnavtmpl) {
                 $(api.args.topnav).innerHTML = riot.render(topnavtmpl.innerHTML.trim(), {
