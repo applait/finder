c6c3e2e951717cacd615c67cdfdbb942ce328878
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-01-22 16:29:24 +0530

    Fix indentation in preferences.js and hoist vars
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/ui/preferences.js
+++ js/ui/preferences.js
@@ -4,47 +4,47 @@
 
         api.on("load:about", function () {
 
-        var debug_pref = $("#debugToggle");
+            var debug_pref = $("#debugToggle"),
+                case_pref = $("#caseToggle"),
+                hidden_pref = $("#hiddenToggle");
+
             !!parseInt(localStorage.getItem("finderDebug")) ?
                 (debug_pref.checked = true) :
                 (debug_pref.checked = false);
-
-        var case_pref = $("#caseToggle");
             !!parseInt(localStorage.getItem("finderCase")) ?
                 (case_pref.checked = true) :
                 (case_pref.checked = false);
 
-        var hidden_pref = $("#hiddenToggle");
             !!parseInt(localStorage.getItem("finderHidden")) ?
                 (hidden_pref.checked = true) :
                 (hidden_pref.checked = false);
 
-        debug_pref.addEventListener("click", function () {
-            if (debug_pref.checked) {
-                localStorage.setItem("finderDebug", "1");
-            } else {
-                localStorage.setItem("finderDebug", "0");
-            }
-        }, false);
+            debug_pref.addEventListener("click", function () {
+                if (debug_pref.checked) {
+                    localStorage.setItem("finderDebug", "1");
+                } else {
+                    localStorage.setItem("finderDebug", "0");
+                }
+            }, false);
 
-        case_pref.addEventListener("click", function () {
-            if (case_pref.checked) {
-                localStorage.setItem("finderCase", "1");
-            } else {
-                localStorage.setItem("finderCase", "0");
-            }
-        }, false);
+            case_pref.addEventListener("click", function () {
+                if (case_pref.checked) {
+                    localStorage.setItem("finderCase", "1");
+                } else {
+                    localStorage.setItem("finderCase", "0");
+                }
+            }, false);
 
-        hidden_pref.addEventListener("click", function () {
-            if (hidden_pref.checked) {
-                localStorage.setItem("finderHidden", "1");
-            } else {
-                localStorage.setItem("finderHidden", "0");
-            }
-        }, false);
+            hidden_pref.addEventListener("click", function () {
+                if (hidden_pref.checked) {
+                    localStorage.setItem("finderHidden", "1");
+                } else {
+                    localStorage.setItem("finderHidden", "0");
+                }
+            }, false);
 
-        // Memory Cleanup
-        debug_pref = case_pref = hidden_pref = null;
+            // Memory Cleanup
+            debug_pref = case_pref = hidden_pref = null;
 
         });
     });
